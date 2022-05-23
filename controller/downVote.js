385f34c1b1a1674960df8d1b4c32952811c59fe2
Saurav15 <saurav.maherchandani@simformsolutions.com>
2022-05-23 19:55:25 +0530

    ..

--- controller/downVote.js
+++ controller/downVote.js
@@ -1,6 +1,9 @@
 // This file will update the downVote count and add the user who downvoted to the list.
 // Also will calculte the total downVote Percentage wrt to total active users.
 // If % > 60%. Remove song from the queue.
-const downVote = (req, res) => {};
+
+const downVote = (req, res) => {
+    
+};
 
 module.exports = downVote;
